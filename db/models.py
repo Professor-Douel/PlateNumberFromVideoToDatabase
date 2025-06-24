0e91691421a3c0369bfd08cf7d0d479c3e39ddc0
Oleksandr Barabanov <oleksandr.barabanov.py@gmail.com>
2025-06-25 01:34:48 +0300

    Add DBPlateNumber model to created file db/models.py

---
+++ db/models.py
@@ -0,0 +1,13 @@
+import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
+
+from db.engine import Base
+
+
+class DBPlateNumber(Base):
+    __tablename__ = 'plate_number'
+
+    id = Column(Integer, primary_key=True, index=True)
+    plate_number = Column(String(20), nullable=False)
+    date_created = Column(DateTime, default=datetime.datetime.utcnow)
