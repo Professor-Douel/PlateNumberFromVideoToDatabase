214960c5ea5b5b3cfc3c2d66f28ce8e74f255ddd
Oleksandr Barabanov <oleksandr.barabanov.py@gmail.com>
2025-06-27 16:58:37 +0300

    Add functions get_all_plate_numbers, get_by_number to the created crud.py

---
+++ crud.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+
+from db import models
+
+
+def get_all_plate_numbers(db: Session):
+    return db.query(models.DBPlateNumber).all()
+
+
+def get_by_number(db: Session, plate_number: str):
+    return db.query(models.DBPlateNumber).filter(
+        models.DBPlateNumber.plate_number == plate_number
+    ).first()
